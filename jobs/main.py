c94add5d99b5cd6d520bb9fba3974f31e2679e4e
Megumi Hirao <komegi.blog@gmail.com>
2024-05-03 22:09:37 +0900

    laid out all the methods to call the data

--- jobs/main.py
+++ jobs/main.py
@@ -1,11 +1,13 @@
 import os
 import random
+import uuid
+
 from confluent_kafka import SerializingProducer
 import simplejson as json
-from datetime import datetime
+from datetime import datetime, timedelta
 
-LONDON_COORDIMATES = { "latitude": 51.5074, "longitude" -0.1278}
-BIRMINGHAM_COORDIMATES = { "latitude": 52.4862, "longitude" -1.8904}
+LONDON_COORDIMATES = { "latitude": 51.5074, "longitude": -0.1278}
+BIRMINGHAM_COORDIMATES = { "latitude": 52.4862, "longitude": -1.8904}
 
 #CALCULATE movement increments
 LATITUDE_INCREMENT = (BIRMINGHAM_COORDIMATES['latitude'] - LONDON_COORDIMATES['latitude'])/100
@@ -18,9 +20,59 @@ GPS_TOPIC= os.getenv('GPS_TOPIC', 'gps_data')
 TRAFFIC_TOPIC = os.getenv('TRAFFIC_TOPIC', 'traffic_data')
 WEATHER_TOPIC = os.getenv('WEATHER_TOPIC', 'weather_data')
 EMERGENCY_TOPIC = os.getenv('EMERGENCY_TOPIC', 'emergency_data')
+random.seed(42)
 
 start_time = datetime.now()
 start_location = LONDON_COORDIMATES.copy()
+
+def get_next_time():
+    global start_time
+    start_time += timedelta(seconds= random.randint(30, 60))
+    return start_time
+
+def generate_gps_data(device_id, timestamp, vehicle_type = 'private'):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'timestamp': timestamp,
+        'speed': random.uniform(0,40),
+        'direction': 'North-East',
+        'vehicleType': vehicle_type
+    }
+
+def generate_traffic_camera_data(device_id, timestamp, camera_id):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'cameraId': camera_id,
+        'timestamp': timestamp,
+        'snapshot': 'Base64EncodedString'
+    }
+def generate_weather_data(device_id, timestamp, location):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'location': location,
+        'timestamp': timestamp,
+        'temperature': random.uniform(-5, 26),
+        'weatherCondition': random.choice(['Sunny', 'Cloudy', 'Rainy', 'Snowy']),
+        'precipitation': random.uniform(0,25),
+        'windSpeed': random.uniform(0, 100),
+        'humidity': random.randint(0, 100), #percentage
+        'AirQualityIndex': random.uniform(0, 500)
+    }
+
+def generate_emergency_incident_data(device_id, timestamp, location):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'incidentId': uuid.uuid4(),
+        'type': random.choice(['Accident', 'Fire', 'Medical', 'Police', 'None']),
+        'timestamp': timestamp,
+        'location': location,
+        'status': random.choice(['Active', 'Resolved']),
+        'description': 'Description of the incident'
+    }
 
 def simulate_vehicle_movement():
     global start_location
@@ -36,13 +88,35 @@ def simulate_vehicle_movement():
 
 def generate_vehicle_data(device_id):
     location = simulate_vehicle_movement()
+    return {
+        'id': uuid.uuid4(),
+        'deviceID': device_id,
+        'timestamp': get_next_time().isoformat(),
+        'location': (location['latitude'], location['longitude']),
+        'speed': random.uniform(10,40),
+        'direction': 'North-East',
+        'make': 'BMW',
+        'model': 'C500',
+        'year': 2024,
+        'fuelType': 'Hybrid'
+    }
 
 
-def simulate_journey(produver, device_id):
+def simulate_journey(producer, device_id):
     while True:
         vehicle_data = generate_vehicle_data(device_id)
+        gps_data = generate_gps_data(device_id, vehicle_data['timestamp'])
+        traffic_camera_data = generate_traffic_camera_data(device_id, vehicle_data['timestamp'], 'Nikon-Camera123')
+        weather_data = generate_weather_data(device_id, vehicle_data['timestamp'], vehicle_data['location'])
+        emergency_incident_data = generate_emergency_incident_data(device_id, vehicle_data['timestamp'], vehicle_data['location'])
+        print(vehicle_data)
+        print(traffic_camera_data)
+        print(traffic_camera_data)
+        print(weather_data)
+        print(emergency_incident_data)
+        break
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     producer_config = {
         'bootstrap.servers': KAFKA_BOOTSTRAP_SERVERS,
         'error_cb': lambda err: print(f'Kafka error: {err}')
